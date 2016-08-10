948cfbdf97c4e305e18bd69f347a246c1d55b2d0
Raymund Delfin <raymund@ultro.co.uk>
2016-08-11 06:09:47 +0800

    Fix scss version

--- package.js
+++ package.js
@@ -1,20 +1,20 @@
 Package.describe({
-  name: 'themeteorchef:bert',
-  version: '2.1.1',
+  name: 'rcdelfin:bert',
+  version: '2.1.2',
   summary: 'A client side, multi-style alerts system for Meteor.',
-  git: 'http://github.com/themeteorchef/bert',
+  git: 'https://github.com/rcdelfin/bert.git',
   documentation: 'README.md'
 });
 
 Package.onUse( function( api ) {
-  api.versionsFrom( '1.2.0.2' );
+  api.versionsFrom( '1.3.5.1' );
 
   api.use([
     'ecmascript',
     'templating',
     'session',
     'jquery',
-    'fourseven:scss@3.8.0_1',
+    'fourseven:scss@3.8.0',
     'fortawesome:fontawesome@4.4.0'
   ], 'client');
 
@@ -33,6 +33,6 @@ Package.onUse( function( api ) {
 
 Package.onTest(function (api) {
   api.use( [ 'tinytest', 'session', 'jquery' ] );
-  api.use( 'themeteorchef:bert' );
+  api.use( 'rcdelfin:bert' );
   api.addFiles( 'tests/client.js', 'client' );
 });
